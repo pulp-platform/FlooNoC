e0a153ac82e258fcd49d722943591abba86fe8a2
Tim Fischer <fischeti@iis.ee.ethz.ch>
2024-01-03 16:33:46 +0100

    refactor: Fix more include folders

--- hw/test/axi_reorder_remap_compare.sv
+++ hw/test/axi_reorder_remap_compare.sv
@@ -31,7 +31,7 @@ module axi_reorder_remap_compare #(
   output logic end_of_sim_o
 );
 
-`include "axi_print_txns.svh"
+`include "include/axi_print_txns.svh"
 
 aw_chan_t aw_queue_sent [NumAxiInIds-1:0][$];
 aw_chan_t aw_queue_recv [NumAxiOutIds-1:0][$];
