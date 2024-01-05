0be807be76a9b145d2c8380eeeb364d77a500098
Tim Fischer <fischeti@iis.ee.ethz.ch>
2024-01-05 20:37:14 +0100

    util(jobs): Re-add removed python shabang

--- util/gen_jobs.py
+++ util/gen_jobs.py
@@ -1,10 +1,10 @@
+#!/usr/bin/env python3
 # Copyright 2024 ETH Zurich and University of Bologna.
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Tim Fischer <fischeti@iis.ee.ethz.ch>
 
-"""Randomly generates job files."""
 import random
 import argparse
 import os
