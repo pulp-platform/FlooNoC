236eb4509583f91dcb5fb949700264e70e16f2c5
Tim Fischer <fischeti@iis.ee.ethz.ch>
2024-01-05 10:31:32 +0100

    hw(pkg): Regenerate the SV packages

--- hw/floo_axi_pkg.sv
+++ hw/floo_axi_pkg.sv
@@ -4,7 +4,6 @@
 //
 // This file is auto-generated. Do not edit! Edit the template file instead
 
-
 `include "axi/typedef.svh"
 
 package floo_axi_pkg;
@@ -24,94 +23,84 @@ package floo_axi_pkg;
     NumAxiChannels = 5
   } axi_ch_e;
 
-  localparam int unsigned AxiInAddrWidth = 32;
+
+  localparam int unsigned AxiInAddrWidth = 48;
   localparam int unsigned AxiInDataWidth = 64;
-  localparam int unsigned AxiInIdWidth = 3;
+  localparam int unsigned AxiInIdWidth = 4;
   localparam int unsigned AxiInUserWidth = 1;
 
-  localparam int unsigned AxiOutAddrWidth = 32;
+
+  localparam int unsigned AxiOutAddrWidth = 48;
   localparam int unsigned AxiOutDataWidth = 64;
-  localparam int unsigned AxiOutIdWidth = 3;
+  localparam int unsigned AxiOutIdWidth = 2;
   localparam int unsigned AxiOutUserWidth = 1;
 
-  typedef logic [AxiInAddrWidth-1:0] axi_in_addr_t;
-  typedef logic [AxiInDataWidth-1:0] axi_in_data_t;
-  typedef logic [AxiInDataWidth/8-1:0] axi_in_strb_t;
-  typedef logic [AxiInIdWidth-1:0] axi_in_id_t;
-  typedef logic [AxiInUserWidth-1:0] axi_in_user_t;
 
-  typedef logic [AxiOutAddrWidth-1:0] axi_out_addr_t;
-  typedef logic [AxiOutDataWidth-1:0] axi_out_data_t;
-  typedef logic [AxiOutDataWidth/8-1:0] axi_out_strb_t;
-  typedef logic [AxiOutIdWidth-1:0] axi_out_id_t;
-  typedef logic [AxiOutUserWidth-1:0] axi_out_user_t;
-
+  typedef logic [47:0] axi_in_addr_t;
+  typedef logic [63:0] axi_in_data_t;
+  typedef logic [7:0] axi_in_strb_t;
+  typedef logic [3:0] axi_in_id_t;
+  typedef logic [0:0] axi_in_user_t;
   `AXI_TYPEDEF_ALL_CT(axi_in, axi_in_req_t, axi_in_rsp_t, axi_in_addr_t, axi_in_id_t, axi_in_data_t,
                       axi_in_strb_t, axi_in_user_t)
+
+
+  typedef logic [47:0] axi_out_addr_t;
+  typedef logic [63:0] axi_out_data_t;
+  typedef logic [7:0] axi_out_strb_t;
+  typedef logic [1:0] axi_out_id_t;
+  typedef logic [0:0] axi_out_user_t;
   `AXI_TYPEDEF_ALL_CT(axi_out, axi_out_req_t, axi_out_rsp_t, axi_out_addr_t, axi_out_id_t,
                       axi_out_data_t, axi_out_strb_t, axi_out_user_t)
+
+
 
   /////////////////////////
   //   Header Typedefs   //
   /////////////////////////
 
   localparam route_algo_e RouteAlgo = XYRouting;
-  typedef logic [8:0] rob_idx_t;
+  localparam int unsigned NumXBits = 2;
+  localparam int unsigned NumYBits = 2;
+  localparam int unsigned XAddrOffset = 12;
+  localparam int unsigned YAddrOffset = 14;
 
-  localparam int unsigned NumXBits = 3;
-  localparam int unsigned NumYBits = 3;
-  localparam int unsigned XAddrOffset = 16;
-  localparam int unsigned YAddrOffset = 19;
 
+  typedef logic [3:0] rob_idx_t;
+  typedef logic [1:0] x_bits_t;
+  typedef logic [1:0] y_bits_t;
   typedef struct packed {
-    logic [NumXBits-1:0] x;
-    logic [NumYBits-1:0] y;
-  } xy_id_t;
+    x_bits_t x;
+    y_bits_t y;
+  } id_t;
 
-  function automatic logic [NumXBits-1:0] get_x_coord(logic [31:0] addr);
-    return addr[XAddrOffset+:NumXBits];
-  endfunction
 
-  function automatic logic [NumYBits-1:0] get_y_coord(logic [31:0] addr);
-    return addr[YAddrOffset+:NumYBits];
-  endfunction
-
-  function automatic xy_id_t get_xy_id(logic [31:0] addr);
-    xy_id_t id;
-    id.x = get_x_coord(addr);
-    id.y = get_y_coord(addr);
-    return id;
-  endfunction
-
-  function automatic logic [31:0] get_base_addr(xy_id_t id);
-    logic [31:0] addr;
-    addr = id.x << XAddrOffset + id.y << YAddrOffset;
-    return addr;
-  endfunction
 
   typedef struct packed {
     logic rob_req;
     rob_idx_t rob_idx;
-    xy_id_t dst_id;
-    xy_id_t src_id;
+    id_t dst_id;
+    id_t src_id;
     logic last;
     logic atop;
     axi_ch_e axi_ch;
   } hdr_t;
 
-  ////////////////////////////
-  //   AXI Flits Typedefs   //
-  ////////////////////////////
+
+
+  ////////////////////////
+  //   Flits Typedefs   //
+  ////////////////////////
 
   typedef struct packed {
     hdr_t hdr;
     axi_in_aw_chan_t aw;
-    logic [2:0] rsvd;
   } floo_axi_aw_flit_t;
 
   typedef struct packed {
     hdr_t hdr;
     axi_in_w_chan_t w;
+    logic [13:0] rsvd;
   } floo_axi_w_flit_t;
 
   typedef struct packed {
@@ -123,7 +112,7 @@ package floo_axi_pkg;
   typedef struct packed {
     hdr_t hdr;
     axi_in_ar_chan_t ar;
-    logic [8:0] rsvd;
+    logic [5:0] rsvd;
   } floo_axi_ar_flit_t;
 
   typedef struct packed {
@@ -131,20 +120,16 @@ package floo_axi_pkg;
     axi_in_r_chan_t r;
   } floo_axi_r_flit_t;
 
-
-  ////////////////////////////////
-  //   Generic Flits Typedefs   //
-  ////////////////////////////////
-
   typedef struct packed {
     hdr_t hdr;
-    logic [73:0] rsvd;
+    logic [87:0] rsvd;
   } floo_req_generic_flit_t;
 
   typedef struct packed {
     hdr_t hdr;
-    logic [70:0] rsvd;
+    logic [71:0] rsvd;
   } floo_rsp_generic_flit_t;
+
 
 
   //////////////////////////
@@ -164,6 +149,8 @@ package floo_axi_pkg;
     floo_rsp_generic_flit_t generic;
   } floo_rsp_chan_t;
 
+
+
   ///////////////////////
   //   Link Typedefs   //
   ///////////////////////
@@ -180,5 +167,5 @@ package floo_axi_pkg;
     floo_rsp_chan_t rsp;
   } floo_rsp_t;
 
-endpackage
 
+endpackage
--- hw/floo_narrow_wide_pkg.sv
+++ hw/floo_narrow_wide_pkg.sv
@@ -4,7 +4,6 @@
 //
 // This file is auto-generated. Do not edit! Edit the template file instead
 
-
 `include "axi/typedef.svh"
 
 package floo_narrow_wide_pkg;
@@ -29,113 +28,107 @@ package floo_narrow_wide_pkg;
     NumAxiChannels = 10
   } axi_ch_e;
 
-  localparam int unsigned NarrowInAddrWidth = 48;
-  localparam int unsigned NarrowInDataWidth = 64;
-  localparam int unsigned NarrowInIdWidth = 4;
-  localparam int unsigned NarrowInUserWidth = 5;
 
-  localparam int unsigned NarrowOutAddrWidth = 48;
-  localparam int unsigned NarrowOutDataWidth = 64;
-  localparam int unsigned NarrowOutIdWidth = 2;
-  localparam int unsigned NarrowOutUserWidth = 5;
+  localparam int unsigned AxiNarrowInAddrWidth = 48;
+  localparam int unsigned AxiNarrowInDataWidth = 64;
+  localparam int unsigned AxiNarrowInIdWidth = 4;
+  localparam int unsigned AxiNarrowInUserWidth = 1;
 
-  localparam int unsigned WideInAddrWidth = 48;
-  localparam int unsigned WideInDataWidth = 512;
-  localparam int unsigned WideInIdWidth = 3;
-  localparam int unsigned WideInUserWidth = 1;
 
-  localparam int unsigned WideOutAddrWidth = 48;
-  localparam int unsigned WideOutDataWidth = 512;
-  localparam int unsigned WideOutIdWidth = 1;
-  localparam int unsigned WideOutUserWidth = 1;
+  localparam int unsigned AxiNarrowOutAddrWidth = 48;
+  localparam int unsigned AxiNarrowOutDataWidth = 64;
+  localparam int unsigned AxiNarrowOutIdWidth = 2;
+  localparam int unsigned AxiNarrowOutUserWidth = 1;
 
-  typedef logic [NarrowInAddrWidth-1:0] axi_narrow_in_addr_t;
-  typedef logic [NarrowInDataWidth-1:0] axi_narrow_in_data_t;
-  typedef logic [NarrowInDataWidth/8-1:0] axi_narrow_in_strb_t;
-  typedef logic [NarrowInIdWidth-1:0] axi_narrow_in_id_t;
-  typedef logic [NarrowInUserWidth-1:0] axi_narrow_in_user_t;
 
-  typedef logic [NarrowOutAddrWidth-1:0] axi_narrow_out_addr_t;
-  typedef logic [NarrowOutDataWidth-1:0] axi_narrow_out_data_t;
-  typedef logic [NarrowOutDataWidth/8-1:0] axi_narrow_out_strb_t;
-  typedef logic [NarrowOutIdWidth-1:0] axi_narrow_out_id_t;
-  typedef logic [NarrowOutUserWidth-1:0] axi_narrow_out_user_t;
+  localparam int unsigned AxiWideInAddrWidth = 48;
+  localparam int unsigned AxiWideInDataWidth = 512;
+  localparam int unsigned AxiWideInIdWidth = 3;
+  localparam int unsigned AxiWideInUserWidth = 1;
 
-  typedef logic [WideInAddrWidth-1:0] axi_wide_in_addr_t;
-  typedef logic [WideInDataWidth-1:0] axi_wide_in_data_t;
-  typedef logic [WideInDataWidth/8-1:0] axi_wide_in_strb_t;
-  typedef logic [WideInIdWidth-1:0] axi_wide_in_id_t;
-  typedef logic [WideInUserWidth-1:0] axi_wide_in_user_t;
 
-  typedef logic [WideOutAddrWidth-1:0] axi_wide_out_addr_t;
-  typedef logic [WideOutDataWidth-1:0] axi_wide_out_data_t;
-  typedef logic [WideOutDataWidth/8-1:0] axi_wide_out_strb_t;
-  typedef logic [WideOutIdWidth-1:0] axi_wide_out_id_t;
-  typedef logic [WideOutUserWidth-1:0] axi_wide_out_user_t;
+  localparam int unsigned AxiWideOutAddrWidth = 48;
+  localparam int unsigned AxiWideOutDataWidth = 512;
+  localparam int unsigned AxiWideOutIdWidth = 1;
+  localparam int unsigned AxiWideOutUserWidth = 1;
 
+
+  typedef logic [47:0] axi_narrow_in_addr_t;
+  typedef logic [63:0] axi_narrow_in_data_t;
+  typedef logic [7:0] axi_narrow_in_strb_t;
+  typedef logic [3:0] axi_narrow_in_id_t;
+  typedef logic [0:0] axi_narrow_in_user_t;
   `AXI_TYPEDEF_ALL_CT(axi_narrow_in, axi_narrow_in_req_t, axi_narrow_in_rsp_t, axi_narrow_in_addr_t,
                       axi_narrow_in_id_t, axi_narrow_in_data_t, axi_narrow_in_strb_t,
                       axi_narrow_in_user_t)
+
+
+  typedef logic [47:0] axi_narrow_out_addr_t;
+  typedef logic [63:0] axi_narrow_out_data_t;
+  typedef logic [7:0] axi_narrow_out_strb_t;
+  typedef logic [1:0] axi_narrow_out_id_t;
+  typedef logic [0:0] axi_narrow_out_user_t;
   `AXI_TYPEDEF_ALL_CT(axi_narrow_out, axi_narrow_out_req_t, axi_narrow_out_rsp_t,
                       axi_narrow_out_addr_t, axi_narrow_out_id_t, axi_narrow_out_data_t,
                       axi_narrow_out_strb_t, axi_narrow_out_user_t)
+
+
+  typedef logic [47:0] axi_wide_in_addr_t;
+  typedef logic [511:0] axi_wide_in_data_t;
+  typedef logic [63:0] axi_wide_in_strb_t;
+  typedef logic [2:0] axi_wide_in_id_t;
+  typedef logic [0:0] axi_wide_in_user_t;
   `AXI_TYPEDEF_ALL_CT(axi_wide_in, axi_wide_in_req_t, axi_wide_in_rsp_t, axi_wide_in_addr_t,
                       axi_wide_in_id_t, axi_wide_in_data_t, axi_wide_in_strb_t, axi_wide_in_user_t)
+
+
+  typedef logic [47:0] axi_wide_out_addr_t;
+  typedef logic [511:0] axi_wide_out_data_t;
+  typedef logic [63:0] axi_wide_out_strb_t;
+  typedef logic [0:0] axi_wide_out_id_t;
+  typedef logic [0:0] axi_wide_out_user_t;
   `AXI_TYPEDEF_ALL_CT(axi_wide_out, axi_wide_out_req_t, axi_wide_out_rsp_t, axi_wide_out_addr_t,
                       axi_wide_out_id_t, axi_wide_out_data_t, axi_wide_out_strb_t,
                       axi_wide_out_user_t)
+
+
 
   /////////////////////////
   //   Header Typedefs   //
   /////////////////////////
 
   localparam route_algo_e RouteAlgo = XYRouting;
-  typedef logic [8:0] rob_idx_t;
+  localparam int unsigned NumXBits = 2;
+  localparam int unsigned NumYBits = 2;
+  localparam int unsigned XAddrOffset = 12;
+  localparam int unsigned YAddrOffset = 14;
 
-  localparam int unsigned NumXBits = 3;
-  localparam int unsigned NumYBits = 3;
-  localparam int unsigned XAddrOffset = 36;
-  localparam int unsigned YAddrOffset = 39;
 
+  typedef logic [3:0] rob_idx_t;
+  typedef logic [1:0] x_bits_t;
+  typedef logic [1:0] y_bits_t;
   typedef struct packed {
-    logic [NumXBits-1:0] x;
-    logic [NumYBits-1:0] y;
-  } xy_id_t;
+    x_bits_t x;
+    y_bits_t y;
+  } id_t;
 
-  function automatic logic [NumXBits-1:0] get_x_coord(logic [47:0] addr);
-    return addr[XAddrOffset+:NumXBits];
-  endfunction
 
-  function automatic logic [NumYBits-1:0] get_y_coord(logic [47:0] addr);
-    return addr[YAddrOffset+:NumYBits];
-  endfunction
-
-  function automatic xy_id_t get_xy_id(logic [47:0] addr);
-    xy_id_t id;
-    id.x = get_x_coord(addr);
-    id.y = get_y_coord(addr);
-    return id;
-  endfunction
-
-  function automatic logic [47:0] get_base_addr(xy_id_t id);
-    logic [47:0] addr;
-    addr = id.x << XAddrOffset + id.y << YAddrOffset;
-    return addr;
-  endfunction
 
   typedef struct packed {
     logic rob_req;
     rob_idx_t rob_idx;
-    xy_id_t dst_id;
-    xy_id_t src_id;
+    id_t dst_id;
+    id_t src_id;
     logic last;
     logic atop;
     axi_ch_e axi_ch;
   } hdr_t;
 
-  ////////////////////////////
-  //   AXI Flits Typedefs   //
-  ////////////////////////////
+
+
+  ////////////////////////
+  //   Flits Typedefs   //
+  ////////////////////////
 
   typedef struct packed {
     hdr_t hdr;
@@ -168,7 +161,7 @@ package floo_narrow_wide_pkg;
   typedef struct packed {
     hdr_t hdr;
     axi_wide_in_aw_chan_t aw;
-    logic [4:0] rsvd;
+    logic [0:0] rsvd;
   } floo_wide_aw_flit_t;
 
   typedef struct packed {
@@ -179,13 +172,13 @@ package floo_narrow_wide_pkg;
   typedef struct packed {
     hdr_t hdr;
     axi_wide_in_b_chan_t b;
-    logic [69:0] rsvd;
+    logic [65:0] rsvd;
   } floo_wide_b_flit_t;
 
   typedef struct packed {
     hdr_t hdr;
     axi_wide_in_ar_chan_t ar;
-    logic [10:0] rsvd;
+    logic [6:0] rsvd;
   } floo_wide_ar_flit_t;
 
   typedef struct packed {
@@ -194,25 +187,21 @@ package floo_narrow_wide_pkg;
     logic [58:0] rsvd;
   } floo_wide_r_flit_t;
 
-
-  ////////////////////////////////
-  //   Generic Flits Typedefs   //
-  ////////////////////////////////
-
   typedef struct packed {
     hdr_t hdr;
-    logic [91:0] rsvd;
+    logic [87:0] rsvd;
   } floo_req_generic_flit_t;
 
   typedef struct packed {
     hdr_t hdr;
-    logic [75:0] rsvd;
+    logic [71:0] rsvd;
   } floo_rsp_generic_flit_t;
 
   typedef struct packed {
     hdr_t hdr;
     logic [577:0] rsvd;
   } floo_wide_generic_flit_t;
+
 
 
   //////////////////////////
@@ -241,6 +230,8 @@ package floo_narrow_wide_pkg;
     floo_wide_generic_flit_t generic;
   } floo_wide_chan_t;
 
+
+
   ///////////////////////
   //   Link Typedefs   //
   ///////////////////////
@@ -263,5 +254,5 @@ package floo_narrow_wide_pkg;
     floo_wide_chan_t wide;
   } floo_wide_t;
 
-endpackage
 
+endpackage
