5cce47f74f1dd51cee45e4343d2eec8101b71ed6
Tim Fischer <fischeti@iis.ee.ethz.ch>
2024-01-05 10:09:34 +0100

    floogen(org): Move utils to `floogen` folder

--- floogen/utils.py
+++ floogen/utils.py
@@ -7,18 +7,28 @@
 
 import math
 
-def cdiv(x, y):
+
+def cdiv(x, y) -> int:
     """Returns the ceiling of x/y."""
     return -(-x // y)
 
-def clog2(x):
+
+def clog2(x) -> int:
     """Returns the ceiling of log2(x)."""
     return math.ceil(math.log2(x))
 
-def camel_to_snake(name):
+
+def camel_to_snake(name: str) -> str:
     """Converts a camel case string to snake case."""
     return "".join(["_" + i.lower() if i.isupper() else i for i in name]).lstrip("_")
 
-def snake_to_camel(name):
+
+def snake_to_camel(name: str) -> str:
     """Converts a snake case string to camel case."""
     return "".join([i.capitalize() for i in name.split("_")])
+
+
+def short_dir(direction: str) -> str:
+    """Returns the short direction string."""
+    return "in" if direction == "input" else "out"
+
