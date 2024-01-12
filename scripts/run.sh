8f9c2feacaf2954b54c12dedf0bed3bb923a7333
Tim Fischer <fischeti@iis.ee.ethz.ch>
2024-01-12 16:40:11 +0100

    lint: License header in script files

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Copyright 2024 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
 if [ -z "$VSIM" ]; then
   VSIM="vsim"
 fi
