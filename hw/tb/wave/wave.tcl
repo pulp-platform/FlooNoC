a9765010b31b06ac4b9d689b39960b781333fb3c
Tim Fischer <fischeti@iis.ee.ethz.ch>
2024-01-16 17:42:04 +0100

    lint(wave): Add missing license header

--- hw/tb/wave/wave.tcl
+++ hw/tb/wave/wave.tcl
@@ -1,3 +1,7 @@
+# Copyright 2024 ETH Zurich and University of Bologna.
+# Solderpad Hardware License, Version 0.51, see LICENSE for details.
+# SPDX-License-Identifier: SHL-0.51
+
 proc floo_wave_init {} {
     onerror {resume}
     quietly WaveActivateNextPane {} 0
