e9a80602b0d1964f2fa2b89b89664ce7c1f1e596
sem24f5 Lukas Berner (bernerl) <sem24f5@badile36.ee.ethz.ch>
2024-03-04 08:58:18 +0100

    Adjustments to follow coding guidelines and hopefully pass linting checks

--- hw/vc_router_util/floo_mux.sv
+++ hw/vc_router_util/floo_mux.sv
@@ -1,31 +1,32 @@
+// Copyright 2024 ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+//
+// Lukas Berner <bernerl@student.ethz.ch>
 
 // a simple one-hot encoded multiplexer
 module floo_mux
 #(
-  parameter int unsigned SOURCE_COUNT = 2,
-  parameter int unsigned DATA_WIDTH = 1
+  parameter int unsigned NumInputs = 2,
+  parameter int unsigned DataWidth = 1
 )
 (
-  input logic[SOURCE_COUNT-1:0] sel_i,
-  input logic[SOURCE_COUNT-1:0][DATA_WIDTH-1:0] data_i,
-  output logic[DATA_WIDTH-1:0] data_o
+  input logic[NumInputs-1:0] sel_i,
+  input logic[NumInputs-1:0][DataWidth-1:0] data_i,
+  output logic[DataWidth-1:0] data_o
 );
 
-  logic[DATA_WIDTH-1:0][SOURCE_COUNT-1:0] transposed_data;
-  logic[DATA_WIDTH-1:0][SOURCE_COUNT-1:0] select_mat;
+  logic[DataWidth-1:0][NumInputs-1:0] transposed_data;
+  logic[DataWidth-1:0][NumInputs-1:0] select_mat;
 
-  generate
-    for(genvar i = 0 ; i < DATA_WIDTH; i++) begin
-      for(genvar j = 0 ; j < SOURCE_COUNT; j++) begin
-        assign transposed_data[i][j] = data_i[j][i];
-      end
+  for(genvar i = 0 ; i < DataWidth; i++) begin
+    for(genvar j = 0 ; j < NumInputs; j++) begin
+      assign transposed_data[i][j] = data_i[j][i];
     end
-  endgenerate
-  
-  generate
-    for(genvar i = 0; i < DATA_WIDTH; i++) begin
-      assign data_o[i] = |(transposed_data[i] & sel_i);
-    end
-  endgenerate
+  end
 
-endmodule
+  for(genvar i = 0; i < DataWidth; i++) begin
+    assign data_o[i] = |(transposed_data[i] & sel_i);
+  end
+
+endmodule 
--- hw/vc_router_util/floo_rr_arbiter.sv
+++ hw/vc_router_util/floo_rr_arbiter.sv
@@ -1,74 +1,77 @@
+// Copyright 2024 ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+//
+// Lukas Berner <bernerl@student.ethz.ch>
 
 // one hot & id encoded round robin arbiter
 module floo_rr_arbiter #(
-  parameter N_INPUT = 2,
-  localparam int unsigned N_INPUT_WIDTH = N_INPUT > 1 ? $clog2(N_INPUT) : 1,
+  parameter NumInputs = 2,
+  localparam int NumInputsWidth = NumInputs > 1 ? $clog2(NumInputs) : 1,
 ) (
-  input   logic [N_INPUT-1:0] req_i,
+  input   logic [NumInputs-1:0] req_i,
   input   logic               update_i, //only update arbiter when told to
-  output  logic [N_INPUT-1:0] grant_o,
-  output  logic [N_INPUT_WIDTH-1:0] grant_id_o,
+  output  logic [NumInputs-1:0] grant_o,
+  output  logic [NumInputsWidth-1:0] grant_id_o,
   input   logic               rst_ni, clk_i
 ); 
   genvar i, j;
-  generate
-    if(N_INPUT == 1) begin: 
-    
-      assign grant_o     = req_i;
-      assign grant_id_o = 0;
-    end else begin: 
+  if(NumInputs == 1) begin: 
+  
+    assign grant_o     = req_i;
+    assign grant_id_o = 0;
+  end else begin: 
 
-      logic [N_INPUT-1:0] reodered_req, reordered_selected_req;
-      logic [N_INPUT*2-1:0] left_rotate_helper, right_rotate_helper;
-      logic [N_INPUT-1:0] dereordered_selected_req;
-      logic [N_INPUT_WIDTH-1:0] round_ptr_q, round_ptr_d;
-      logic [N_INPUT_WIDTH-1:0] round_ptr_q_comp;
-      logic [N_INPUT_WIDTH-1:0] selected_req_id;
+    logic [NumInputs-1:0] reodered_req, reordered_selected_req;
+    logic [NumInputs*2-1:0] left_rotate_helper, right_rotate_helper;
+    logic [NumInputs-1:0] dereordered_selected_req;
+    logic [NumInputsWidth-1:0] round_ptr_q, round_ptr_d;
+    logic [NumInputsWidth-1:0] round_ptr_q_comp;
+    logic [NumInputsWidth-1:0] selected_req_id;
 
-      logic [N_INPUT_WIDTH-1:0][N_INPUT-1:0] id_mask;   
+    logic [NumInputsWidth-1:0][NumInputs-1:0] id_mask;   
 
-      // rotade left
-      assign left_rotate_helper = {req_i, req_i} << round_ptr_q;
-      assign reodered_req = left_rotate_helper[N_INPUT*2-1-:N_INPUT];
-          
-      //isolate rightmost 1 bit: reodered_req & (-reodered_req)
-      assign reordered_selected_req = reodered_req & (~reodered_req + 1'b1);
+    // rotade left
+    assign left_rotate_helper = {req_i, req_i} << round_ptr_q;
+    assign reodered_req = left_rotate_helper[NumInputs*2-1-:NumInputs];
+        
+    //isolate rightmost 1 bit: reodered_req & (-reodered_req)
+    assign reordered_selected_req = reodered_req & (~reodered_req + 1'b1);
 
-      //rotate back: circular rotate by N_INPUT - round_ptr_q
-      assign round_ptr_q_comp = N_INPUT - round_ptr_q;
-      assign right_rotate_helper = {reordered_selected_req, reordered_selected_req} << round_ptr_q_comp;
-      assign dereordered_selected_req = right_rotate_helper[N_INPUT*2-1-:N_INPUT];
+    //rotate back: circular rotate by NumInputs - round_ptr_q
+    assign round_ptr_q_comp = NumInputs - round_ptr_q;
+    assign right_rotate_helper = {reordered_selected_req, reordered_selected_req} << round_ptr_q_comp;
+    assign dereordered_selected_req = right_rotate_helper[NumInputs*2-1-:NumInputs];
 
-      //extract id from onehot: create id mask
-      for(i = 0; i < N_INPUT_WIDTH; i++) begin
-        for(j = 0; j < N_INPUT; j++) begin 
-          assign id_mask[i][j] = (j/(2**i)) % 2;
-        end
+    //extract id from onehot: create id mask
+    for(i = 0; i < NumInputsWidth; i++) begin
+      for(j = 0; j < NumInputs; j++) begin 
+        assign id_mask[i][j] = (j/(2**i)) % 2;
       end
-      //mask looks like this: N_Input = 3: (0,0) is first bit
-      // 0 0 0  // 1 0 0  // 0 1 0  // 1 1 0  // 0 0 1  // 1 0 1  // 0 1 1  // 1 1 1  
-      // use mask to get req_id
-      for(i = 0; i < N_INPUT_WIDTH; i++) begin
-        assign selected_req_id[i] = |(dereordered_selected_req & id_mask[i]);
-      end
-
-      // ff for round ptr
-      assign round_ptr_d =  (selected_req_id == '0)          ? N_INPUT-1 :
-                            (selected_req_id == (N_INPUT-1)) ? '0        : (N_INPUT-1) - selected_req_id;
-
-      always_ff @(posedge clk or negedge rst_n) begin
-        if (~rstn) begin
-          round_ptr_q <= '0;
-        end else begin
-          if (update_i) begin
-            round_ptr_q <= round_ptr_d;
-          end
-        end
-      end
-
-      assign grant_o     = dereordered_selected_req;
-      assign grant_id_o  = selected_req_id;
-
     end
-  endgenerate
+    //mask looks like this: N_Input = 3: (0,0) is first bit
+    // 0 0 0  // 1 0 0  // 0 1 0  // 1 1 0  // 0 0 1  // 1 0 1  // 0 1 1  // 1 1 1  
+    // use mask to get req_id
+    for(i = 0; i < NumInputsWidth; i++) begin
+      assign selected_req_id[i] = |(dereordered_selected_req & id_mask[i]);
+    end
+
+    // ff for round ptr
+    assign round_ptr_d =  (selected_req_id == '0)          ? NumInputs-1 :
+                          (selected_req_id == (NumInputs-1)) ? '0        : (NumInputs-1) - selected_req_id;
+
+    always_ff @(posedge clk or negedge rst_n) begin
+      if (~rstn) begin
+        round_ptr_q <= '0;
+      end else begin
+        if (update_i) begin
+          round_ptr_q <= round_ptr_d;
+        end
+      end
+    end
+
+    assign grant_o     = dereordered_selected_req;
+    assign grant_id_o  = selected_req_id;
+
+  end
 endmodule
