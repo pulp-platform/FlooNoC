0364f3a5bbab21fa890e42976f2e5d398a50a4ec
sem24f5 Lukas Berner (bernerl) <sem24f5@badile36.ee.ethz.ch>
2024-03-04 08:58:18 +0100

    Adjustments to follow coding guidelines and hopefully pass linting checks

--- hw/vc_router_util/floo_mux.sv
+++ hw/vc_router_util/floo_mux.sv
@@ -3,11 +3,6 @@
 // SPDX-License-Identifier: SHL-0.51
 //
 // Lukas Berner <bernerl@student.ethz.ch>
-// Copyright 2024 ETH Zurich and University of Bologna.
-// Solderpad Hardware License, Version 0.51, see LICENSE for details.
-// SPDX-License-Identifier: SHL-0.51
-//
-// Lukas Berner <bernerl@student.ethz.ch>
 
 // a simple one-hot encoded multiplexer
 module floo_mux #(
