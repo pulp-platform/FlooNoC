de40ee0f37f3e16cdc7d25935d049c2608e7e3d6
Lukas Berner <burinir.berner@gmail.com>
2024-05-12 13:34:24 +0200

    visualize_traffic: add still image visualization

--- util/visualize_traffic.py
+++ util/visualize_traffic.py
@@ -11,6 +11,7 @@ import argparse
 import subprocess
 import os
 import pygame as pg
+import pygame.gfxdraw
 
 ANIMATION_LENGTH_SECONDS = 2
 NUM_FRAMES = ANIMATION_LENGTH_SECONDS * 20
@@ -21,6 +22,7 @@ IMG_WIDTH = 400
 # parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--out_dir", type=str, default="util/traffic_visualizations")
+parser.add_argument("--gif", type=int, default=0)
 parser.add_argument("--num_x", type=int, default=4)
 parser.add_argument("--num_y", type=int, default=4)
 parser.add_argument("--traffic_type", type=str, default="all")
@@ -104,20 +106,22 @@ def gen_mesh_traffic():
             jobs.append((src_addr, dst_addr))
     return jobs
 
+IMG_BORDER = 30
+GRID_SIZE_X = IMG_WIDTH - 2*IMG_BORDER
+GRID_SIZE_Y = IMG_HEIGHT - 2*IMG_BORDER
 def get_position(x, y):
-    """Get the position of a point in the mesh"""
+    """Get the image coords of a point in the mesh"""
     # input x,y is from 0,0 to NUM_X+1, NUM_Y+1
-    # output is from 30,30 to 370,370
-    return (x*340/(NUM_X+1)+30, y*340/(NUM_Y+1)+30)
+    # output is in image coords
+    return (x*GRID_SIZE_X/(NUM_X+1)+IMG_BORDER, y*GRID_SIZE_Y/(NUM_Y+1)+IMG_BORDER)
 
 
-def visualize_traffic():
+def visualize_traffic_gif():
     """generate gif showing the traffic"""
     # pylint: disable=too-many-locals, consider-using-in, too-many-boolean-expressions
     # Make and save a drawing for each frame
     filename_list = [os.path.join(args.out_dir, 'temp' + str(n) + '.png')
                          for n in range(NUM_FRAMES)]
-
     # get the jobs
     jobs = gen_mesh_traffic()
 
@@ -125,15 +129,14 @@ def visualize_traffic():
         # initialize surface
         surf = pg.Surface((IMG_WIDTH,IMG_HEIGHT))
         surf.fill((255,255,255))
-
         # draw the mesh
         for x in range(NUM_X+2):
             for y in range(NUM_Y+2):
                 line_color = (100,100,100)
                 if x != NUM_X+1 and y != 0 and y != NUM_Y+1:
-                    pg.draw.line(surf, line_color, get_position(x,y), get_position(x+1,y), 2)
+                    pg.draw.line(surf, line_color, get_position(x,y), get_position(x+1,y), 1)
                 if y != NUM_Y+1 and x != 0 and x != NUM_X+1 :
-                    pg.draw.line(surf, line_color, get_position(x,y), get_position(x,y+1), 2)
+                    pg.draw.line(surf, line_color, get_position(x,y), get_position(x,y+1), 1)
         for x in range(NUM_X+2):
             for y in range(NUM_Y+2):
                 node_color = (0,0,0)
@@ -143,7 +146,6 @@ def visualize_traffic():
                     node_color = (150,150,150)
                 pg.draw.circle(surf, node_color, get_position(x,y), 10)
 
-        # pg.draw.circle(surf, color, (x, y), radius, 0)
         # for each job calculate current position of circle to be drawn and draw it
         for job in jobs:
             factor = frame / NUM_FRAMES # 0 to 1
@@ -154,10 +156,6 @@ def visualize_traffic():
             coord = (job[1][0] * factor + job[0][0] * (1-factor),
                      job[1][1] * factor + job[0][1] * (1-factor))
             pg.draw.circle(surf, (255,0,0), get_position(coord[0], coord[1]), 4)
-
-
-        # scale down then save the surface
-        #shrunk_surface = pg.transform.smoothscale(surf, (final_width, final_height))
         pg.image.save(surf, filename_list[frame])
 
     seconds_per_frame = ANIMATION_LENGTH_SECONDS / NUM_FRAMES
@@ -168,9 +166,81 @@ def visualize_traffic():
     # Use the "convert" command (part of ImageMagick) to build the animation
     subprocess.call(command_list)
     # Earlier, we saved an image file for each frame of the animation. Now
-    # that the animation is assembled, we can (optionally) delete those files
+    # that the animation is assembled, we can delete those files
     for filename in filename_list:
         os.remove(filename)
+
+def visualize_traffic_image():
+    """generate still image showing the traffic"""
+    # pylint: disable=too-many-locals, c-extension-no-member, too-many-boolean-expressions, consider-using-in
+    # get the jobs
+    jobs = gen_mesh_traffic()
+    # initialize surface
+    surf = pg.Surface((IMG_WIDTH,IMG_HEIGHT))
+    surf.fill((255,255,255))
+    # draw the mesh
+    for x in range(NUM_X+2):
+        for y in range(NUM_Y+2):
+            line_color = (100,100,100)
+            if x != NUM_X+1 and y != 0 and y != NUM_Y+1:
+                pg.draw.line(surf, line_color, get_position(x,y), get_position(x+1,y), 1)
+            if y != NUM_Y+1 and x != 0 and x != NUM_X+1 :
+                pg.draw.line(surf, line_color, get_position(x,y), get_position(x,y+1), 1)
+    for x in range(NUM_X+2):
+        for y in range(NUM_Y+2):
+            node_color = (0,0,0)
+            if x == 0 and (y == 0 or y == NUM_Y+1) or x == NUM_X+1 and (y == 0 or y == NUM_Y+1):
+                continue
+            if x == 0 or x == NUM_X+1 or y == 0 or y == NUM_Y+1:
+                node_color = (150,150,150)
+            pg.draw.circle(surf, node_color, get_position(x,y), 10)
+    #arrow parameterd
+    # minangle = 0, mindist = 1
+    maxangle = math.pi/7 # at maxdist
+    maxdist = math.sqrt(NUM_X**2 + NUM_Y**2)
+    arrow_color = (255,0,0)
+    arrow_tip_angle = math.pi/6
+    arrow_tip_length = 7
+    # for each job draw a arrow
+    for job in jobs:
+        if job[0][0] == job[1][0] and job[0][1] == job[1][1]:
+            continue
+        dist = math.sqrt((job[1][0] - job[0][0])**2 + (job[1][1] - job[0][1])**2)
+        # starting and ending angle of arrow (as measured from connecting line)
+        angle = (dist-1)*maxangle/(maxdist-1)
+        line_direction = (job[1][0]-job[0][0], job[1][1]-job[0][1])
+        line_angle = math.atan2(line_direction[1], line_direction[0])
+        third_point_distance = dist/2/math.cos(angle)
+        mid_point = get_position(job[0][0] + third_point_distance*math.cos(line_angle+angle),
+                    job[0][1] + third_point_distance*math.sin(line_angle+angle))
+        first_point = (get_position(job[0][0], job[0][1])[0]+math.cos(angle+line_angle)*10,
+                      get_position(job[0][0], job[0][1])[1]+math.sin(angle+line_angle)*10)
+        last_point = (get_position(job[1][0], job[1][1])[0]+math.cos(math.pi-angle+line_angle)*10,
+                      get_position(job[1][0], job[1][1])[1]+math.sin(math.pi-angle+line_angle)*10)
+        if dist==1:
+            pg.draw.line(surf, arrow_color, first_point, last_point, 1)
+        else:
+            pygame.gfxdraw.bezier(surf, (first_point,
+                                 mid_point,
+                                 last_point),
+                                 20, arrow_color)
+        # draw arrow head: last_point is tip, angle is arrow_tip_angle
+        pg.draw.polygon(surf, arrow_color, [last_point,
+                (last_point[0] - arrow_tip_length*math.cos(-arrow_tip_angle+line_angle-angle),
+                last_point[1] - arrow_tip_length*math.sin(-arrow_tip_angle+line_angle-angle)),
+                (last_point[0] - arrow_tip_length*math.cos(arrow_tip_angle+line_angle-angle),
+                last_point[1] - arrow_tip_length*math.sin(arrow_tip_angle+line_angle-angle))])
+    output_filename = os.path.join(args.out_dir, args.traffic_type +'.png')
+    pg.image.save(surf, output_filename)
+
+
+def visualize_traffic():
+    """visualize traffic"""
+    if args.gif:
+        visualize_traffic_gif()
+    else:
+        visualize_traffic_image()
+
 
 def main():
     """Main function."""
