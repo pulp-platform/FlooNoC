4b69104416b1137b6b3401a8e1e6bc18f0897cdb
Tim Fischer <fischeti@iis.ee.ethz.ch>
2024-09-18 10:43:23 +0200

    tb: Add `nw_mesh` testbench with generated NoC

---
+++ hw/tb/wave/tb_floo_nw_mesh.wave.tcl
@@ -0,0 +1,31 @@
+# Copyright 2023 ETH Zurich and University of Bologna.
+# Solderpad Hardware License, Version 0.51, see LICENSE for details.
+# SPDX-License-Identifier: SHL-0.51
+
+source hw/tb/wave/wave.tcl
+
+floo_wave_init
+
+set tb_name tb_floo_nw_mesh
+set noc_name i_floo_nw_mesh_noc
+
+set routers [find instances -bydu floo_nw_router -nodu]
+set num_y [regexp -all {router_0} $routers]
+set num_x [expr {[llength $routers] / $num_y}]
+
+for {set y 0} {$y < $num_y} {incr y} {
+  for {set x 0} {$x < $num_x} {incr x} {
+    set groups [list Node X=${x} Y=${y}]
+    floo_narrow_wide_chimney_wave $tb_name/$noc_name/cluster_ni_${x}_${y} [concat $groups [list Chimney]]
+    floo_router_wave $tb_name/$noc_name/router_${x}_${y} [concat $groups [list Router]]
+
+    floo_add_wave $tb_name/gen_x[$x]/gen_y[$y]/i_axi_narrow_bw_monitor/*in_flight_o $groups 1
+    floo_add_wave $tb_name/gen_x[$x]/gen_y[$y]/i_axi_wide_bw_monitor/*in_flight_o $groups 1
+  }
+}
+
+for {set y 0} {$y < $num_y} {incr y} {
+  floo_narrow_wide_chimney_wave $tb_name/$noc_name/hbm_ni_${y} [list HBM West "Channel ${y}" Chimney] 1
+}
+
+floo_wave_style
