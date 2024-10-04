2fc84f11295e6843f93a28580e8483fc02ceddf4
Yvan Tortorella <ytortorella@lagrev5.ee.ethz.ch>
2024-10-04 20:42:03 +0200

    Move Astral template from IDTable to Source routing.

--- hw/astral/floo_astral_noc.sv
+++ hw/astral/floo_astral_noc.sv
@@ -33,7 +33,7 @@ package floo_astral_noc_pkg;
   typedef logic[0:0] rob_idx_t;
 typedef logic[0:0] port_id_t;
 typedef logic[3:0] id_t;
-typedef logic route_t;
+typedef logic[3:0] route_t;
 
 
   localparam int unsigned SamNumRules = 7;
@@ -56,14 +56,137 @@ localparam sam_rule_t[SamNumRules-1:0] Sam = '{
 };
 
 
+  localparam route_t[NumEndpoints-1:0][NumEndpoints-1:0] RoutingTables = '{
+'{
+4'b????,// -> peripherals_ni
+4'b0001,// -> opentitan_main_ni
+4'b0010,// -> opentitan_dma_ni
+4'b????,// -> mbox_ni
+4'b????,// -> l2_port1_ni
+4'b????,// -> l2_port0_ni
+4'b0110,// -> ethernet_ni
+4'b1001,// -> dram_ni
+4'b0101,// -> cluster_ni
+4'b0000 // -> cheshire_ni
+},
+'{
+4'b1000,// -> peripherals_ni
+4'b????,// -> opentitan_main_ni
+4'b????,// -> opentitan_dma_ni
+4'b0111,// -> mbox_ni
+4'b0100,// -> l2_port1_ni
+4'b0011,// -> l2_port0_ni
+4'b????,// -> ethernet_ni
+4'b1001,// -> dram_ni
+4'b0101,// -> cluster_ni
+4'b0000 // -> cheshire_ni
+},
+'{
+4'b1000,// -> peripherals_ni
+4'b????,// -> opentitan_main_ni
+4'b????,// -> opentitan_dma_ni
+4'b0111,// -> mbox_ni
+4'b0100,// -> l2_port1_ni
+4'b0011,// -> l2_port0_ni
+4'b????,// -> ethernet_ni
+4'b1001,// -> dram_ni
+4'b0101,// -> cluster_ni
+4'b0000 // -> cheshire_ni
+},
+'{
+4'b????,// -> peripherals_ni
+4'b0001,// -> opentitan_main_ni
+4'b0010,// -> opentitan_dma_ni
+4'b????,// -> mbox_ni
+4'b????,// -> l2_port1_ni
+4'b????,// -> l2_port0_ni
+4'b0110,// -> ethernet_ni
+4'b1001,// -> dram_ni
+4'b0101,// -> cluster_ni
+4'b0000 // -> cheshire_ni
+},
+'{
+4'b????,// -> peripherals_ni
+4'b0001,// -> opentitan_main_ni
+4'b0010,// -> opentitan_dma_ni
+4'b????,// -> mbox_ni
+4'b????,// -> l2_port1_ni
+4'b????,// -> l2_port0_ni
+4'b0110,// -> ethernet_ni
+4'b1001,// -> dram_ni
+4'b0101,// -> cluster_ni
+4'b0000 // -> cheshire_ni
+},
+'{
+4'b????,// -> peripherals_ni
+4'b0001,// -> opentitan_main_ni
+4'b0010,// -> opentitan_dma_ni
+4'b????,// -> mbox_ni
+4'b????,// -> l2_port1_ni
+4'b????,// -> l2_port0_ni
+4'b0110,// -> ethernet_ni
+4'b1001,// -> dram_ni
+4'b0101,// -> cluster_ni
+4'b0000 // -> cheshire_ni
+},
+'{
+4'b1000,// -> peripherals_ni
+4'b????,// -> opentitan_main_ni
+4'b????,// -> opentitan_dma_ni
+4'b0111,// -> mbox_ni
+4'b0100,// -> l2_port1_ni
+4'b0011,// -> l2_port0_ni
+4'b????,// -> ethernet_ni
+4'b1001,// -> dram_ni
+4'b0101,// -> cluster_ni
+4'b0000 // -> cheshire_ni
+},
+'{
+4'b1000,// -> peripherals_ni
+4'b0001,// -> opentitan_main_ni
+4'b0010,// -> opentitan_dma_ni
+4'b0111,// -> mbox_ni
+4'b0100,// -> l2_port1_ni
+4'b0011,// -> l2_port0_ni
+4'b0110,// -> ethernet_ni
+4'b????,// -> dram_ni
+4'b0101,// -> cluster_ni
+4'b0000 // -> cheshire_ni
+},
+'{
+4'b1000,// -> peripherals_ni
+4'b0001,// -> opentitan_main_ni
+4'b0010,// -> opentitan_dma_ni
+4'b0111,// -> mbox_ni
+4'b0100,// -> l2_port1_ni
+4'b0011,// -> l2_port0_ni
+4'b0110,// -> ethernet_ni
+4'b1001,// -> dram_ni
+4'b????,// -> cluster_ni
+4'b0000 // -> cheshire_ni
+},
+'{
+4'b1000,// -> peripherals_ni
+4'b0001,// -> opentitan_main_ni
+4'b0010,// -> opentitan_dma_ni
+4'b0111,// -> mbox_ni
+4'b0100,// -> l2_port1_ni
+4'b0011,// -> l2_port0_ni
+4'b0110,// -> ethernet_ni
+4'b1001,// -> dram_ni
+4'b0101,// -> cluster_ni
+4'b???? // -> cheshire_ni
+}}
+;
 
-  localparam route_cfg_t RouteCfg = '{    RouteAlgo: IdTable,
+
+  localparam route_cfg_t RouteCfg = '{    RouteAlgo: SourceRouting,
     UseIdTable: 1'b1,
     XYAddrOffsetX: 0,
     XYAddrOffsetY: 0,
     IdAddrOffset: 0,
     NumSamRules: 7,
-    NumRoutes: 0};
+    NumRoutes: 10};
 
 
   typedef logic[47:0] in_axi_in_addr_t;
@@ -83,7 +206,7 @@ typedef logic[9:0] out_axi_out_user_t;
 
 
 
-  `FLOO_TYPEDEF_HDR_T(hdr_t, id_t, id_t, axi_ch_e, rob_idx_t)
+  `FLOO_TYPEDEF_HDR_T(hdr_t, route_t, id_t, axi_ch_e, rob_idx_t)
   localparam axi_cfg_t AxiCfg = '{    AddrWidth: 48,
     DataWidth: 64,
     UserWidth: 10,
@@ -200,6 +323,8 @@ floo_axi_chimney  #(
   .RouteCfg(RouteCfg),
   .id_t(id_t),
   .rob_idx_t(rob_idx_t),
+  .route_t (route_t),
+  .dst_t   (route_t),
   .hdr_t  (hdr_t),
   .sam_rule_t(sam_rule_t),
   .Sam(Sam),
@@ -219,7 +344,7 @@ floo_axi_chimney  #(
   .axi_out_req_o ( cheshire_axi_out_req_o ),
   .axi_out_rsp_i ( cheshire_axi_out_rsp_i ),
   .id_i             ( id_t'(0) ),
-  .route_table_i    ( '0                          ),
+  .route_table_i    ( RoutingTables[CheshireNi]  ),
   .floo_req_o       ( cheshire_ni_to_router_req   ),
   .floo_rsp_i       ( router_to_cheshire_ni_rsp   ),
   .floo_req_i       ( router_to_cheshire_ni_req   ),
@@ -232,6 +357,8 @@ floo_axi_chimney  #(
   .RouteCfg(RouteCfg),
   .id_t(id_t),
   .rob_idx_t(rob_idx_t),
+  .route_t (route_t),
+  .dst_t   (route_t),
   .hdr_t  (hdr_t),
   .sam_rule_t(sam_rule_t),
   .Sam(Sam),
@@ -251,7 +378,7 @@ floo_axi_chimney  #(
   .axi_out_req_o (    ),
   .axi_out_rsp_i ( '0 ),
   .id_i             ( id_t'(8) ),
-  .route_table_i    ( '0                          ),
+  .route_table_i    ( RoutingTables[OpentitanMainNi]  ),
   .floo_req_o       ( opentitan_main_ni_to_router_req   ),
   .floo_rsp_i       ( router_to_opentitan_main_ni_rsp   ),
   .floo_req_i       ( router_to_opentitan_main_ni_req   ),
@@ -264,6 +391,8 @@ floo_axi_chimney  #(
   .RouteCfg(RouteCfg),
   .id_t(id_t),
   .rob_idx_t(rob_idx_t),
+  .route_t (route_t),
+  .dst_t   (route_t),
   .hdr_t  (hdr_t),
   .sam_rule_t(sam_rule_t),
   .Sam(Sam),
@@ -283,7 +412,7 @@ floo_axi_chimney  #(
   .axi_out_req_o (    ),
   .axi_out_rsp_i ( '0 ),
   .id_i             ( id_t'(7) ),
-  .route_table_i    ( '0                          ),
+  .route_table_i    ( RoutingTables[OpentitanDmaNi]  ),
   .floo_req_o       ( opentitan_dma_ni_to_router_req   ),
   .floo_rsp_i       ( router_to_opentitan_dma_ni_rsp   ),
   .floo_req_i       ( router_to_opentitan_dma_ni_req   ),
@@ -296,6 +425,8 @@ floo_axi_chimney  #(
   .RouteCfg(RouteCfg),
   .id_t(id_t),
   .rob_idx_t(rob_idx_t),
+  .route_t (route_t),
+  .dst_t   (route_t),
   .hdr_t  (hdr_t),
   .sam_rule_t(sam_rule_t),
   .Sam(Sam),
@@ -315,7 +446,7 @@ floo_axi_chimney  #(
   .axi_out_req_o ( l2_port0_axi_out_req_o ),
   .axi_out_rsp_i ( l2_port0_axi_out_rsp_i ),
   .id_i             ( id_t'(4) ),
-  .route_table_i    ( '0                          ),
+  .route_table_i    ( RoutingTables[L2Port0Ni]  ),
   .floo_req_o       ( l2_port0_ni_to_router_req   ),
   .floo_rsp_i       ( router_to_l2_port0_ni_rsp   ),
   .floo_req_i       ( router_to_l2_port0_ni_req   ),
@@ -328,6 +459,8 @@ floo_axi_chimney  #(
   .RouteCfg(RouteCfg),
   .id_t(id_t),
   .rob_idx_t(rob_idx_t),
+  .route_t (route_t),
+  .dst_t   (route_t),
   .hdr_t  (hdr_t),
   .sam_rule_t(sam_rule_t),
   .Sam(Sam),
@@ -347,7 +480,7 @@ floo_axi_chimney  #(
   .axi_out_req_o ( l2_port1_axi_out_req_o ),
   .axi_out_rsp_i ( l2_port1_axi_out_rsp_i ),
   .id_i             ( id_t'(5) ),
-  .route_table_i    ( '0                          ),
+  .route_table_i    ( RoutingTables[L2Port1Ni]  ),
   .floo_req_o       ( l2_port1_ni_to_router_req   ),
   .floo_rsp_i       ( router_to_l2_port1_ni_rsp   ),
   .floo_req_i       ( router_to_l2_port1_ni_req   ),
@@ -360,6 +493,8 @@ floo_axi_chimney  #(
   .RouteCfg(RouteCfg),
   .id_t(id_t),
   .rob_idx_t(rob_idx_t),
+  .route_t (route_t),
+  .dst_t   (route_t),
   .hdr_t  (hdr_t),
   .sam_rule_t(sam_rule_t),
   .Sam(Sam),
@@ -379,7 +514,7 @@ floo_axi_chimney  #(
   .axi_out_req_o ( cluster_axi_out_req_o ),
   .axi_out_rsp_i ( cluster_axi_out_rsp_i ),
   .id_i             ( id_t'(1) ),
-  .route_table_i    ( '0                          ),
+  .route_table_i    ( RoutingTables[ClusterNi]  ),
   .floo_req_o       ( cluster_ni_to_router_req   ),
   .floo_rsp_i       ( router_to_cluster_ni_rsp   ),
   .floo_req_i       ( router_to_cluster_ni_req   ),
@@ -392,6 +527,8 @@ floo_axi_chimney  #(
   .RouteCfg(RouteCfg),
   .id_t(id_t),
   .rob_idx_t(rob_idx_t),
+  .route_t (route_t),
+  .dst_t   (route_t),
   .hdr_t  (hdr_t),
   .sam_rule_t(sam_rule_t),
   .Sam(Sam),
@@ -411,7 +548,7 @@ floo_axi_chimney  #(
   .axi_out_req_o (    ),
   .axi_out_rsp_i ( '0 ),
   .id_i             ( id_t'(3) ),
-  .route_table_i    ( '0                          ),
+  .route_table_i    ( RoutingTables[EthernetNi]  ),
   .floo_req_o       ( ethernet_ni_to_router_req   ),
   .floo_rsp_i       ( router_to_ethernet_ni_rsp   ),
   .floo_req_i       ( router_to_ethernet_ni_req   ),
@@ -424,6 +561,8 @@ floo_axi_chimney  #(
   .RouteCfg(RouteCfg),
   .id_t(id_t),
   .rob_idx_t(rob_idx_t),
+  .route_t (route_t),
+  .dst_t   (route_t),
   .hdr_t  (hdr_t),
   .sam_rule_t(sam_rule_t),
   .Sam(Sam),
@@ -443,7 +582,7 @@ floo_axi_chimney  #(
   .axi_out_req_o ( mbox_axi_out_req_o ),
   .axi_out_rsp_i ( mbox_axi_out_rsp_i ),
   .id_i             ( id_t'(6) ),
-  .route_table_i    ( '0                          ),
+  .route_table_i    ( RoutingTables[MboxNi]  ),
   .floo_req_o       ( mbox_ni_to_router_req   ),
   .floo_rsp_i       ( router_to_mbox_ni_rsp   ),
   .floo_req_i       ( router_to_mbox_ni_req   ),
@@ -456,6 +595,8 @@ floo_axi_chimney  #(
   .RouteCfg(RouteCfg),
   .id_t(id_t),
   .rob_idx_t(rob_idx_t),
+  .route_t (route_t),
+  .dst_t   (route_t),
   .hdr_t  (hdr_t),
   .sam_rule_t(sam_rule_t),
   .Sam(Sam),
@@ -475,7 +616,7 @@ floo_axi_chimney  #(
   .axi_out_req_o ( peripherals_axi_out_req_o ),
   .axi_out_rsp_i ( peripherals_axi_out_rsp_i ),
   .id_i             ( id_t'(9) ),
-  .route_table_i    ( '0                          ),
+  .route_table_i    ( RoutingTables[PeripheralsNi]  ),
   .floo_req_o       ( peripherals_ni_to_router_req   ),
   .floo_rsp_i       ( router_to_peripherals_ni_rsp   ),
   .floo_req_i       ( router_to_peripherals_ni_req   ),
@@ -488,6 +629,8 @@ floo_axi_chimney  #(
   .RouteCfg(RouteCfg),
   .id_t(id_t),
   .rob_idx_t(rob_idx_t),
+  .route_t (route_t),
+  .dst_t   (route_t),
   .hdr_t  (hdr_t),
   .sam_rule_t(sam_rule_t),
   .Sam(Sam),
@@ -507,31 +650,12 @@ floo_axi_chimney  #(
   .axi_out_req_o ( dram_axi_out_req_o ),
   .axi_out_rsp_i ( dram_axi_out_rsp_i ),
   .id_i             ( id_t'(2) ),
-  .route_table_i    ( '0                          ),
+  .route_table_i    ( RoutingTables[DramNi]  ),
   .floo_req_o       ( dram_ni_to_router_req   ),
   .floo_rsp_i       ( router_to_dram_ni_rsp   ),
   .floo_req_i       ( router_to_dram_ni_req   ),
   .floo_rsp_o       ( dram_ni_to_router_rsp   )
 );
-
-localparam int unsigned RouterMapNumRules = 7;
-
-typedef struct packed {
-    id_t idx;
-    id_t start_addr;
-    id_t end_addr;
-} router_map_rule_t;
-
-localparam router_map_rule_t[RouterMapNumRules-1:0] RouterMap = '{
-'{idx: 0, start_addr: 0, end_addr: 1},// cheshire_ni
-'{idx: 3, start_addr: 4, end_addr: 5},// l2_port0_ni
-'{idx: 4, start_addr: 5, end_addr: 6},// l2_port1_ni
-'{idx: 5, start_addr: 1, end_addr: 2},// cluster_ni
-'{idx: 7, start_addr: 6, end_addr: 7},// mbox_ni
-'{idx: 8, start_addr: 9, end_addr: 10},// peripherals_ni
-'{idx: 9, start_addr: 2, end_addr: 3} // dram_ni
-
-};
 
 
 floo_req_t [9:0] router_req_in;
@@ -585,7 +709,7 @@ floo_rsp_t [9:0] router_rsp_in;
 
 floo_axi_router #(
   .AxiCfg(AxiCfg),
-  .RouteAlgo (IdTable),
+  .RouteAlgo (SourceRouting),
   .NumRoutes (10),
   .NumInputs (10),
   .NumOutputs (10),
@@ -593,8 +717,6 @@ floo_axi_router #(
   .OutFifoDepth (2),
   .id_t(id_t),
   .hdr_t(hdr_t),
-  .NumAddrRules (7),
-  .addr_rule_t (router_map_rule_t),
   .floo_req_t(floo_req_t),
   .floo_rsp_t(floo_rsp_t)
 ) router (
@@ -602,7 +724,7 @@ floo_axi_router #(
   .rst_ni,
   .test_enable_i,
   .id_i ('0),
-  .id_route_map_i (RouterMap),
+  .id_route_map_i ('0),
   .floo_req_i (router_req_in),
   .floo_rsp_o (router_rsp_out),
   .floo_req_o (router_req_out),
