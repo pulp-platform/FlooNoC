5bbb60b2d423a5554e9cd6facb5448969e5a66a2
Tim Fischer <fischeti@iis.ee.ethz.ch>
2024-10-07 16:41:09 +0200

    floogen(routing): More reasonable ordering of endpoint name

--- floogen/model/graph.py
+++ floogen/model/graph.py
@@ -5,6 +5,7 @@
 #
 # Author: Tim Fischer <fischeti@iis.ee.ethz.ch>
 
+import re
 from typing import List, Tuple
 
 import networkx as nx
@@ -286,7 +287,15 @@ class Graph(nx.DiGraph):  # pylint: disable=too-many-public-methods
     def create_unique_ep_id(self, node) -> int:
         """Return the endpoint id."""
         ep_nodes = [name for name, _ in self.get_ep_nodes(with_name=True)]
-        return sorted(ep_nodes).index(node)
+
+        # Custom sorting function: extract numeric part after letters
+        def extract_number(name):
+            match = re.search(r"(\d+)$", name)
+            return int(match.group(1)) if match else -1
+
+        return sorted(
+            ep_nodes, key=lambda name: (re.sub(r"\d+$", "", name), extract_number(name))
+        ).index(node)
 
     def get_node_id(self, node_name=None, node_obj=None):
         """Return the node id."""
