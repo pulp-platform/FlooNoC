8519263558bc7e10195a36e3c131599df8198f4c
Tim Fischer <fischeti@iis.ee.ethz.ch>
2025-04-09 18:31:05 +0200

    hw(red_sync): Clean up `reduction_sync`

--- hw/floo_reduction_sync.sv
+++ hw/floo_reduction_sync.sv
@@ -5,50 +5,45 @@
 // Author: Chen Wu <chenwu@student.ethz.ch>
 
 module floo_reduction_sync import floo_pkg::*;
-# (
+#(
+  /// Number of input ports
   parameter int unsigned NumRoutes  = 1,
+  /// Type definitions
   parameter type         arb_idx_t  = logic,
   parameter type         flit_t     = logic,
   parameter type         id_t       = logic
 ) (
-  input  arb_idx_t               index,
+  input  arb_idx_t               sel_i,
   input  flit_t [NumRoutes-1:0]  data_i,
   input  logic  [NumRoutes-1:0]  valid_i,
-  input  id_t                    node_id_i,
+  input  id_t                    xy_id_i,
   output logic                   valid_o,
   output logic  [NumRoutes-1:0]  in_route_mask_o
 );
 
   logic [NumRoutes-1:0]  compare_same, same_and_valid;
 
+  // Compute the mask of expected input ports to reduce from
   floo_route_xymask #(
     .NumRoutes ( NumRoutes ),
     .flit_t    ( flit_t    ),
     .id_t      ( id_t      ),
-    .Mode      ( 0         ) // Mode 0 for spefifying the expected input directions
+    .FwdMode   ( 0         ) // We enable the backward mode for reduction
   ) i_route_xymask (
-    .channel_i ( data_i[index] ),
-    .xy_id_i   ( node_id_i     ),
-    .route_sel_o( in_route_mask_o )
+    .channel_i    ( data_i[sel_i]   ),
+    .xy_id_i      ( xy_id_i         ),
+    .route_sel_o  ( in_route_mask_o )
   );
 
-  id_t mask_in, src_id, dst_id;
-  assign mask_in = id_t'(data_i[index].hdr.mask);
-  assign src_id = id_t'(data_i[index].hdr.src_id);
-  assign dst_id = id_t'(data_i[index].hdr.dst_id);
-
-  always_comb begin
-    for (int in_route = 0; in_route < NumRoutes; in_route++) begin
-      id_t current_src_id, current_mask;
-      current_src_id = id_t'(data_i[in_route].hdr.src_id);
-      current_mask = id_t'(data_i[in_route].hdr.mask);
-      // only compare whether the same mask and dst_id
-      compare_same[in_route] = ((current_mask == mask_in)
-                                && (id_t'(data_i[in_route].hdr.dst_id) == dst_id));
-      same_and_valid[in_route] = (dst_id == node_id_i && in_route == Eject)?
-                                1 : (compare_same[in_route] & valid_i[in_route]);
-    end
-    valid_o = in_route_mask_o=='0? 0 : &(same_and_valid | ~in_route_mask_o);
+  for (genvar in = 0; in < NumRoutes; in++) begin : gen_routes
+    // Compare whether the `mask` and `dst_id` are equal to the selected input port
+    assign compare_same[in] = ((data_i[in].hdr.mask == data_i[sel_i].hdr.mask) &&
+                               (data_i[in].hdr.dst_id == data_i[sel_i].hdr.dst_id));
+    // TODO(fischeti): Check with Chen what is done here
+    assign same_and_valid[in] = (data_i[sel_i].hdr.dst_id == xy_id_i && in == Eject) ||
+                                (compare_same[in] & valid_i[in]);
   end
+
+  assign valid_o = (in_route_mask_o == '0)? 1'b0 : &(same_and_valid | ~in_route_mask_o);
 
 endmodule
