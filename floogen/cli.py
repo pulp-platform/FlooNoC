ddcd9a7cbbaddd60b2ddf614919213e79e7bf544
Tim Fischer <fischeti@iis.ee.ethz.ch>
2025-12-14 15:32:18 +0100

    floogen: Refactor CLI with subcommands + add `template` command for custom/external templates (#155)
    * Add tile-based network generation
    
    Add --tile-based CLI flag to generate tile-wrapped network output:
    - floo_{name}_noc.sv: Top-level with tile grid instantiation
    - floo_{name}_tile.sv: Tile wrapper (chimney + router)
    
    This enables cleaner RTL output for regular mesh topologies where
    each tile contains a router and network interface.
    
    New files:
    - floogen/templates/floo_axi_tile.sv.mako
    - floogen/templates/floo_top_noc_tiles.sv.mako
    
    * floogen: Replace --tile-based with generic --template flag
    
    Address PR review feedback from @fischeti:
    
    - Replace --tile-based boolean flag with -t/--template that accepts
      external Mako template paths. Can be specified multiple times.
    - Templates are loaded dynamically at runtime and receive the 'noc'
      Network object as context
    - Output filename derived from template stem: floo_{name}_{stem}.sv
    
    Refactor tile templates to use direction enums:
    
    - Use [West:North] for external ports (4 cardinal directions)
    - Use [Eject:North] for internal router signals (5 ports)
    - Simplify router connections using generate loop
    - Remove redundant wire declarations and mappings in top template
    
    Usage:
      floogen -c config.yml -o out/       -t floogen/templates/floo_axi_tile.sv.mako       -t floogen/templates/floo_top_noc_tiles.sv.mako
    
    * Fixed broken reference to RouteAlgo
    
    * floogen(cli): Restructure CLI with subcommands
    
    * floogen(templates): Rename and add new templates
    
    * ci: Use new floogen CLI
    
    * py: Track `.python-version` file and use `3.14`
    
    * pd: Bump commit
    
    * ci: Add check to test python versions
    
    * ci: Fix path
    
    * ci: Cache pip deps in Gitlab CI
    
    * ci: Use project-based cache directory
    
    * docs: Update README
    
    * floogen: Rename `all` target to `rtl`
    
    * floogen(cli): Move up `template` in target order
    
    * README: Remove list of IPs
    
    Is not interesting and not really up to date
    
    * pd: Revert to master branch
    
    * floogen(cli): Fix some comments
    
    * floogen: Remove custom template
    
    ---------
    
    Co-authored-by: Kevin Greig <kgreig@tenstorrent.com>

--- floogen/cli.py
+++ floogen/cli.py
@@ -5,148 +5,190 @@
 #
 # Author: Tim Fischer <fischeti@iis.ee.ethz.ch>
 
-import os
 import argparse
 from pathlib import Path
+from importlib.resources import files
+
+from mako.template import Template
 
 from floogen.config_parser import parse_config
 from floogen.query import handle_query
 from floogen.model.network import Network
 from floogen.utils import verible_format
 
+tpl_dir = files("floogen") / "templates"
 
-# pylint: disable=too-many-branches
-def render_sources(network: Network, args: argparse.Namespace):
-    """Render the sources for the network."""
-
-    # Create the output directory if it doesn't exist
-    if args.outdir:
-        outdir = Path(args.outdir)
-        if not outdir.is_absolute():
-            outdir = Path(os.getcwd(), outdir)
-        outdir.mkdir(parents=True, exist_ok=True)
-
-    # Visualize the network graph
-    if args.visualize:
-        if args.outdir:
-            network.visualize(filename=outdir / (network.name + ".pdf"))
+def render_template(context: dict, tpl: Path,
+                    outdir: Path = None, file_name: str = None,
+                    format_output: bool=False, verible_fmt_bin: str = None,
+                    verible_fmt_args: str = None):
+    """Render a template, format if requested and write to file or print to stdout."""
+    if not tpl.exists():
+        # Search in the internal template directory if the template exists there
+        if (tpl_dir / tpl.name).exists():
+            tpl = tpl_dir / tpl.name
         else:
-            network.visualize(savefig=False)
-
-    # Generate the network description
-    rendered_pkg = network.render_package()
-    rendered_top = network.render_network()
-
-    # Format the output if requested
-    if not args.no_format:
-        rendered_top = verible_format(rendered_top, args.verible_fmt_bin, args.verible_fmt_args)
-        rendered_pkg = verible_format(rendered_pkg, args.verible_fmt_bin, args.verible_fmt_args)
-
-    # Write the network description to file or print it to stdout
-    if args.outdir:
-        if not args.only_top and not args.rdl:
-            pkg_file_name = outdir / f"floo_{network.name}_noc_pkg.sv"
-            with open(pkg_file_name, "w+", encoding="utf-8") as pkg_file:
-                pkg_file.write(rendered_pkg)
-        if not args.only_pkg and not args.rdl:
-            top_file_name = outdir / f"floo_{network.name}_noc.sv"
-            with open(top_file_name, "w+", encoding="utf-8") as top_file:
-                top_file.write(rendered_top)
-        if args.rdl:
-            rdl_file_name = outdir / f"{network.name}.rdl"
-            with open(rdl_file_name, "w+", encoding="utf-8") as rdl_file:
-                rdl_file.write(network.render_rdl(rdl_as_mem=args.rdl_as_mem,
-                                                  rdl_memwidth=args.rdl_memwidth))
+            raise FileNotFoundError(f"Template not found: {tpl}")
+    rendered = Template(filename=str(tpl.resolve())).render(**context)
+    if format_output:
+        rendered = verible_format(rendered, verible_fmt_bin, verible_fmt_args)
+    if outdir:
+        outdir.mkdir(parents=True, exist_ok=True)
+        if file_name:
+            outfile = outdir / file_name
+        else:
+            outfile = outdir / tpl.stem
+        with open(outfile, "w+", encoding="utf-8") as f:
+            f.write(rendered)
     else:
-        if not args.only_top and not args.rdl:
-            print(rendered_pkg)
-        if not args.only_pkg and not args.rdl:
-            print(rendered_top)
-        if args.rdl:
-            print(network.render_rdl(rdl_as_mem=args.rdl_as_mem, rdl_memwidth=args.rdl_memwidth))
+        print(rendered)
 
 
-def parse_args():
+def build_parser() -> argparse.ArgumentParser:
     """Parse the command line arguments."""
-    parser = argparse.ArgumentParser(description="FlooGen: A Network-on-Chip Generator")
-    parser.add_argument(
-        "-c", "--config", type=Path, required=True, help="Path to the configuration file."
+
+    # Parser that holds all common options
+    common = argparse.ArgumentParser(add_help=False)
+    common.add_argument(
+        "-c", "--config", type=Path, required=True,
+        help="Path to the configuration file."
     )
-    parser.add_argument(
-        "-o",
-        "--outdir",
-        type=Path,
-        required=False,
+    common.add_argument(
+        "-o", "--outdir", type=Path, required=False,
         help=(
             "Path to the output directory of the generated output files. "
             "If not specified, the files are printed to stdout."
         ),
     )
-    parser.add_argument(
-        "--only-pkg",
-        dest="only_pkg",
-        action="store_true",
-        default=False,
-        help="Only generate the NoC package.",
-    )
-    parser.add_argument(
-        "--only-top",
-        dest="only_top",
-        action="store_true",
-        default=False,
-        help="Only generate the NoC top-module.",
-    )
-    parser.add_argument(
-        "--rdl",
-        dest="rdl",
-        action="store_true",
-        default=False,
-        help="Generate the system's RDL.",
-    )
-    parser.add_argument(
-        "--rdl-as-mem",
-        dest="rdl_as_mem",
-        action="store_true",
-        default=False,
-        help="Add memory blocks for address regions without 'rdl_name' declared.",
-    )
-    parser.add_argument(
-        "--rdl-memwidth",
-        dest="rdl_memwidth",
-        type=int,
-        default=8,
-        help="Use the memory width of the RDL address region as the width of the memory block.",
-    )
-    parser.add_argument(
+
+    # Parser for SystemVerilog formatting options
+    sv_format = argparse.ArgumentParser(add_help=False)
+    sv_format.add_argument(
         "--no-format",
         dest="no_format",
         action="store_true",
         help="Do not format the output.",
     )
-    parser.add_argument(
+    sv_format.add_argument(
         "--verible-fmt-bin",
         type=str,
         default=None,
         help="Overwrite default `verible-verilog-format` binary.",
     )
-    parser.add_argument(
+    sv_format.add_argument(
         "--verible-fmt-args",
         type=str,
         default=None,
         help="Additional arguments to pass to `verible-verilog-format`.",
     )
-    parser.add_argument("--visualize", action="store_true", help="Visualize the network graph.")
-    parser.add_argument(
-        "-q", "--query", type=str, help="Query a specific key in the configuration."
+    sv_format.add_argument(
+        "--name",
+        type=str,
+        default=None,
+        help="Override the module/package name and prefix for generated files."
     )
 
-    return parser.parse_args()
+    # Top-level parser
+    parser = argparse.ArgumentParser(
+        description="FlooGen: A Network-on-Chip Generator for FlooNoC",
+        add_help=True,
+    )
+
+    subparsers = parser.add_subparsers(dest="command")
+
+    # floogen rtl -> pkg + top
+    subparsers.add_parser(
+        "rtl",
+        parents=[common, sv_format],
+        add_help=True,
+        help="Generate both the NoC package and top-module.",
+    )
+
+    # floogen pkg
+    subparsers.add_parser(
+        "pkg",
+        parents=[common, sv_format],
+        add_help=True,
+        help="Generate the NoC package.",
+    )
+
+    # floogen top
+    subparsers.add_parser(
+        "top",
+        parents=[common, sv_format],
+        add_help=True,
+        help="Generate the NoC top-module.",
+    )
+
+    # floogen rdl
+    p_rdl = subparsers.add_parser(
+        "rdl",
+        parents=[common],
+        add_help=True,
+        help="Generate the SystemRDL of all endpoint address regions.",
+    )
+    p_rdl.add_argument(
+        "--as-mem",
+        dest="as_mem",
+        action="store_true",
+        default=False,
+        help="Add memory blocks for address regions without 'rdl_name' declared.",
+    )
+    p_rdl.add_argument(
+        "--memwidth",
+        dest="memwidth",
+        type=int,
+        default=8,
+        help="Use the memory width of the RDL address region as the width of the memory block.",
+    )
+
+    # floogen templates <template1> <template2> ...
+    p_templates = subparsers.add_parser(
+        "template",
+        parents=[common, sv_format],
+        add_help=True,
+        help="Render custom (external) templates.",
+    )
+    p_templates.add_argument(
+        "template",
+        type=Path,
+        nargs="+",
+        help="Path to external template to render. Multiple templates can be specified.",
+    )
+
+    # floogen visualize
+    subparsers.add_parser(
+        "visualize",
+        parents=[common],
+        add_help=True,
+        help="Visualize the network graph.",
+    )
+
+    # floogen query <key>
+    p_query = subparsers.add_parser(
+        "query",
+        parents=[common],
+        add_help=True,
+        help="Query a specific key in the configuration.",
+    )
+    p_query.add_argument(
+        "query",
+        type=str,
+        help="Key to query in the configuration.",
+    )
+
+    return parser
 
 
 def main():
     """Generates the network."""
 
-    args = parse_args()
+    parser = build_parser()
+    args = parser.parse_args()
+
+    if args.command is None:
+        parser.print_help()
+        return 0
 
     network = parse_config(Network, args.config)
 
@@ -154,10 +196,70 @@ def main():
     network.compile_network()
     network.gen_routing_info()
 
-    if args.query:
-        handle_query(network, args.query)
-    else:
-        render_sources(network, args)
+    # The general context to pass to all templates
+    context = {"noc": network}
+
+    # Additional render arguments
+    render_kwargs = {"outdir": args.outdir}
+
+    # Command specific render arguments
+    match args.command:
+        case "rtl" | "pkg" | "top" | "template":
+            render_kwargs["format_output"] = not args.no_format
+            render_kwargs["verible_fmt_bin"] = args.verible_fmt_bin
+            render_kwargs["verible_fmt_args"] = args.verible_fmt_args
+            context["name"] = args.name or network.name
+            pkg_file_name = f"floo_{args.name or network.name}_noc_pkg.sv"
+            top_file_name = f"floo_{args.name or network.name}_noc.sv"
+        case "rdl":
+            rdl_file_name = f"{network.name}_addrmap.rdl"
+
+
+    match args.command:
+        case "rtl":
+            render_template(context,
+                tpl=tpl_dir / "floo_noc_pkg.sv.mako",
+                file_name=pkg_file_name,
+                **render_kwargs,
+            )
+            render_template(context,
+                tpl=tpl_dir / "floo_noc.sv.mako",
+                file_name=top_file_name,
+                **render_kwargs,
+            )
+        case "pkg":
+            render_template(context,
+                tpl=tpl_dir / "floo_noc_pkg.sv.mako",
+                file_name=pkg_file_name,
+                **render_kwargs,
+            )
+        case "top":
+            render_template(context,
+                tpl=tpl_dir / "floo_noc.sv.mako",
+                file_name=top_file_name,
+                **render_kwargs,
+            )
+        case "rdl":
+            context["rdl_as_mem"] = args.as_mem
+            context["rdl_memwidth"] = args.memwidth
+            render_template(context,
+                tpl=tpl_dir / "floo_addrmap.rdl.mako",
+                file_name=rdl_file_name,
+                **render_kwargs,
+            )
+        case "template":
+            for tpl in args.template:
+                render_template(context,
+                    tpl=tpl,
+                    **render_kwargs,
+                )
+        case "visualize":
+            if args.outdir:
+                network.visualize(filename=args.outdir / (network.name + ".pdf"))
+            else:
+                network.visualize(savefig=False)
+        case "query":
+            handle_query(network, args.query)
 
 
 if __name__ == "__main__":
---
+++ floogen/templates/floo_axi_tile.sv.mako
@@ -0,0 +1,130 @@
+<%!
+    import datetime
+    from floogen.utils import snake_to_camel, bool_to_sv
+    from floogen.model.routing import XYDirections
+%>\
+// Copyright ${datetime.datetime.now().year} ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+
+// AUTOMATICALLY GENERATED! DO NOT EDIT!
+
+module floo_${name}_tile
+  import floo_pkg::*;
+  import floo_${name}_noc_pkg::*;
+#(
+  parameter logic EnSbrPort = 1'b1,
+  parameter logic EnMgrPort = 1'b1
+) (
+  input  logic clk_i,
+  input  logic rst_ni,
+  input  logic test_enable_i,
+
+  // XY coordinate input
+  input  id_t id_i,
+
+  // AXI ports
+<% in_prot = next((prot for prot in noc.protocols if prot.direction == "input"), None) %>\
+<% out_prot = next((prot for prot in noc.protocols if prot.direction == "output"), None) %>\
+% if in_prot is not None:
+  input  ${in_prot.type_name()}_req_t axi_in_req_i,
+  output ${in_prot.type_name()}_rsp_t axi_in_rsp_o,
+% endif
+% if out_prot is not None:
+  output ${out_prot.type_name()}_req_t axi_out_req_o,
+  input  ${out_prot.type_name()}_rsp_t axi_out_rsp_i,
+% endif
+
+  // Router links to neighbors
+  output floo_req_t [West:North] floo_req_o,
+  output floo_rsp_t [West:North] floo_rsp_o,
+  input  floo_req_t [West:North] floo_req_i,
+  input  floo_rsp_t [West:North] floo_rsp_i
+);
+
+  // Internal links between chimney and router (Eject direction)
+  floo_req_t chimney_to_router_req;
+  floo_rsp_t router_to_chimney_rsp;
+  floo_req_t router_to_chimney_req;
+  floo_rsp_t chimney_to_router_rsp;
+
+  // Router internal signals
+  floo_req_t [Eject:North] router_req_in;
+  floo_rsp_t [Eject:North] router_rsp_out;
+  floo_req_t [Eject:North] router_req_out;
+  floo_rsp_t [Eject:North] router_rsp_in;
+
+  // Router connections using direction enums
+  // External directions (North=0, East=1, South=2, West=3) connect to neighbors
+  for (genvar d = North; d <= West; d++) begin : gen_dir_connect
+    assign router_req_in[d] = floo_req_i[d];
+    assign router_rsp_in[d] = floo_rsp_i[d];
+    assign floo_req_o[d] = router_req_out[d];
+    assign floo_rsp_o[d] = router_rsp_out[d];
+  end
+
+  // Eject direction connects to local chimney
+  assign router_req_in[Eject] = chimney_to_router_req;
+  assign router_rsp_in[Eject] = chimney_to_router_rsp;
+  assign router_to_chimney_req = router_req_out[Eject];
+  assign router_to_chimney_rsp = router_rsp_out[Eject];
+
+  // Router instantiation
+  floo_axi_router #(
+    .AxiCfg      ( AxiCfg             ),
+    .RouteAlgo   ( RouteCfg.RouteAlgo ),
+    .NumRoutes   ( 5                  ),
+    .NumInputs   ( 5                  ),
+    .NumOutputs  ( 5                  ),
+    .InFifoDepth ( 2                  ),
+    .OutFifoDepth( 2                  ),
+    .id_t        ( id_t               ),
+    .hdr_t       ( hdr_t              ),
+    .floo_req_t  ( floo_req_t         ),
+    .floo_rsp_t  ( floo_rsp_t         )
+  ) i_router (
+    .clk_i,
+    .rst_ni,
+    .test_enable_i,
+    .id_i           ( id_i          ),
+    .id_route_map_i ( '0            ),
+    .floo_req_i     ( router_req_in ),
+    .floo_rsp_o     ( router_rsp_out),
+    .floo_req_o     ( router_req_out),
+    .floo_rsp_i     ( router_rsp_in )
+  );
+
+  // Chimney (Network Interface) instantiation
+  floo_axi_chimney #(
+    .AxiCfg       ( AxiCfg                                    ),
+    .ChimneyCfg   ( set_ports(ChimneyDefaultCfg, EnSbrPort, EnMgrPort) ),
+    .RouteCfg     ( RouteCfg                                  ),
+    .id_t         ( id_t                                      ),
+    .rob_idx_t    ( rob_idx_t                                 ),
+    .hdr_t        ( hdr_t                                     ),
+    .sam_rule_t   ( sam_rule_t                                ),
+    .Sam          ( Sam                                       ),
+    .axi_in_req_t ( ${in_prot.type_name()}_req_t              ),
+    .axi_in_rsp_t ( ${in_prot.type_name()}_rsp_t              ),
+    .axi_out_req_t( ${out_prot.type_name()}_req_t             ),
+    .axi_out_rsp_t( ${out_prot.type_name()}_rsp_t             ),
+    .floo_req_t   ( floo_req_t                                ),
+    .floo_rsp_t   ( floo_rsp_t                                )
+  ) i_chimney (
+    .clk_i,
+    .rst_ni,
+    .test_enable_i,
+    .sram_cfg_i    ( '0                    ),
+    .axi_in_req_i  ( axi_in_req_i          ),
+    .axi_in_rsp_o  ( axi_in_rsp_o          ),
+    .axi_out_req_o ( axi_out_req_o         ),
+    .axi_out_rsp_i ( axi_out_rsp_i         ),
+    .id_i          ( id_i                  ),
+    .route_table_i ( '0                    ),
+    .floo_req_o    ( chimney_to_router_req ),
+    .floo_rsp_i    ( router_to_chimney_rsp ),
+    .floo_req_i    ( router_to_chimney_req ),
+    .floo_rsp_o    ( chimney_to_router_rsp )
+  );
+
+endmodule
