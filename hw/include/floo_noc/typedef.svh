e00d623283af86a8e73979b3e9650dbe3b3c91ab
Lorenzo Leone <lleone@iis.ee.ethz.ch>
2025-12-16 08:32:25 +0100

    depr: Move deprectaed typedef

--- hw/include/floo_noc/typedef.svh
+++ hw/include/floo_noc/typedef.svh
@@ -316,29 +316,6 @@
   } floo_``name``_t;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// Defines the all the link types with credit-based flow control interface
-// for use with virtual channels
-//
-// Arguments:
-// - name: Name of the link type
-// - chan_name: Name of the channel type to transport
-// - vc_id_t: Type of the virtual channel ID
-//
-// Usage Example:
-// localparam floo_pkg::axi_cfg_t AxiCfg = '{...};
-// `FLOO_TYPEDEF_HDR_T(hdr_t, ...)
-// `FLOO_TYPEDEF_AXI_FROM_CFG(my_axi, AxiCfg)
-// `FLOO_TYPEDEF_AXI_CHAN_ALL(my_axi, req, rsp, my_axi_in, AxiCfg, hdr_t)
-// FLOO_TYPEDEF_LINK_T(vc_req, my_axi)
-`define FLOO_TYPEDEF_VC_LINK_T(name, chan_name, vc_id_t)  \
-  typedef struct packed {                                 \
-    logic valid;                                          \
-    logic credit_v;                                       \
-    vc_id_t credit_id;                                    \
-    floo_``chan_name``_chan_t ``chan_name``;              \
-  } floo_``name``_t;
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
 // Defines the all the link types with ready-valid handshaking interface
 // for a single AXI interface configuration
 //
@@ -410,53 +387,5 @@
   `FLOO_TYPEDEF_VIRT_CHAN_LINK_T(req, req_chan, req_virt_chan, req_virt_chan)                                                            \
   `FLOO_TYPEDEF_VIRT_CHAN_LINK_T(rsp, rsp_chan, 1, 1)                                                                        \
   `FLOO_TYPEDEF_VIRT_CHAN_LINK_T(wide, wide_chan, wide_virt_chan, wide_phys_chan)
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// Defines the all the link types with credit-based flow control interface
-// for a single AXI interface configuration
-//
-// Arguments:
-// - req: Name of the `req` link type
-// - rsp: Name of the `rsp` link type
-// - req_chan: Name of the `req` channel type to transport
-// - rsp_chan: Name of the `rsp` channel type to transport
-// - vc_id_t: Type of the virtual channel ID
-//
-// Usage Example:
-// localparam floo_pkg::axi_cfg_t AxiCfg = '{...};
-// `FLOO_TYPEDEF_HDR_T(hdr_t, ...)
-// `FLOO_TYPEDEF_AXI_FROM_CFG(my_axi, AxiCfg)
-// `FLOO_TYPEDEF_AXI_CHAN_ALL(my_axi, my_req, my_rsp, my_axi_in, AxiCfg, hdr_t)
-// `FLOO_TYPEDEF_VC_AXI_LINK_ALL(vc_req, vc_rsp, my_req, my_rsp)
-`define FLOO_TYPEDEF_VC_AXI_LINK_ALL(req, rsp, req_chan, rsp_chan, vc_id_t) \
-  `FLOO_TYPEDEF_VC_LINK_T(req, req_chan, vc_id_t)                           \
-  `FLOO_TYPEDEF_VC_LINK_T(rsp, rsp_chan, vc_id_t)                           \
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// Defines the all the link types with credit-based flow control interface
-// for a narrow-wide AXI interface configuration
-//
-// Arguments:
-// - req: Name of the `req` link type
-// - rsp: Name of the `rsp` link type
-// - wide: Name of the `wide` link type
-// - req_chan: Name of the `req` channel type to transport
-// - rsp_chan: Name of the `rsp` channel type to transport
-// - wide_chan: Name of the `wide` channel type to transport
-// - vc_id_t: Type of the virtual channel ID
-//
-// Usage Example:
-// localparam floo_pkg::axi_cfg_t AxiCfgN = '{...};
-// localparam floo_pkg::axi_cfg_t AxiCfgW = '{...};
-// `FLOO_TYPEDEF_HDR_T(hdr_t, ...)
-// `FLOO_TYPEDEF_AXI_FROM_CFG(my_narrow_axi, AxiCfgN)
-// `FLOO_TYPEDEF_AXI_FROM_CFG(my_wide_axi, AxiCfgW)
-// `FLOO_TYPEDEF_NW_CHAN_ALL(axi, my_req, my_rsp, my_wide, my_axi_narrow_in, my_axi_wide_in, AxiCfgN, AxiCfgW, hdr_t)
-// `FLOO_TYPEDEF_NW_LINK_ALL(vc_req, vc_rsp, vc_wide, my_req, my_rsp, my_wide)
-`define FLOO_TYPEDEF_VC_NW_LINK_ALL(req, rsp, wide, req_chan, rsp_chan, wide_chan, vc_id_t) \
-  `FLOO_TYPEDEF_VC_LINK_T(req, req_chan, vc_id_t)                                           \
-  `FLOO_TYPEDEF_VC_LINK_T(rsp, rsp_chan, vc_id_t)                                           \
-  `FLOO_TYPEDEF_VC_LINK_T(wide, wide_chan, vc_id_t)
 
 `endif // FLOO_NOC_TYPEDEF_SVH_
